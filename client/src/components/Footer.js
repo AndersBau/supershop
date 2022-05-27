6fa4ba4a815f45af0f234a6961fc4c1859cddcb4
Anderson Bautista <anderbarker86@gmail.com>
2022-05-27 17:24:19 -0400

    import React- boostrap for styling and add container and rows to footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,9 +1,15 @@
 import React from 'react';
-
+import { Container, Row, Col } from 'react-bootstrap';
 const Footer = () => {
   return (
     <footer>
-      <h2>Footer here</h2>
+      <Container>
+        <Row>
+          <Col className ='text-center py-3'>
+            Copyright &copy; supershop
+          </Col>
+        </Row>
+      </Container>
     </footer>
   );
 }
