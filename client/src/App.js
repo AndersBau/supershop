a9f4a2eb6120ad1a941a41e306222246314c4b3b
Anderson Bautista <anderbarker86@gmail.com>
2022-05-27 17:03:07 -0400

    create base of header and footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 function App() {
   return (
     <>
       <h1>Welcome to the Super-Shop</h1>
+      <Header />
+      <Footer/>
     </>
   );
 }
