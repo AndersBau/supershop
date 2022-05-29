08277b29d2d13675a3cec5df186d17cdd0ece8eb
Anderson Bautista <anderbarker86@gmail.com>
2022-05-29 12:24:25 -0400

    setting files for homepage creation

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
   <Container>
-    <Navbar.Brand href="/">SuperSop</Navbar.Brand>
+    <Navbar.Brand href="/">The SuperShop</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="ms-auto">
