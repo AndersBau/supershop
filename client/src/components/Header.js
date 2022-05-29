079f7a1e5600d4d3803673ef2db9d03853f29e84
Anderson Bautista <anderbarker86@gmail.com>
2022-05-28 23:50:02 -0400

    Navbar added

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,20 @@
 import React from 'react';
-// import { Container, Row, Col } from 'react-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 const Header = () => {
   return (
     <header>
-      <h2>Navbar here</h2>
-      <h4>hell</h4>
+      <Navbar bg="light" expand="lg">
+  <Container>
+    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#link">Link</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
     </header>
   );
 }
