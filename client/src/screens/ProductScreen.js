3cf3cd7e5b785a3e2d2d86fd7545146b3fc48a50
Anderson Bautista <anderbarker86@gmail.com>
2022-06-02 23:58:06 -0400

    setted up product screen link

--- client/src/screens/ProductScreen.js
+++ client/src/screens/ProductScreen.js
@@ -1,8 +1,25 @@
-import React from "react";
-
+import React, {useEffect} from "react";
+import { Link, useParams } from "react-router-dom";
+import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
+import Rating from "../components/Rating";
+import products from "../products";
 const ProductScreen = ()=> {
+  const params = useParams();
+  const product = products.find(p => p._id === params.id);
   return (
-    <div>Produt</div>
+    <>
+      <Link className='btn btn-light my-3' to='/'>
+        Go Back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image}></Image>
+        </Col>
+        <Col md={3}>
+
+        </Col>
+      </Row>
+    </>
   )
 }
 
