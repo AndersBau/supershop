711676a9c17e9e07b2b034fe4e4959c33ca2e6a8
Anderson Bautista <anderbarker86@gmail.com>
2022-06-15 00:13:00 -0400

    productList shows on payload but not on the action state

--- client/src/screens/Homescreen.js
+++ client/src/screens/Homescreen.js
@@ -1,21 +1,24 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import axios from 'axios';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
 import { listProducts } from '../store/actions/productActions';
 
 const HomeScreen = () => {
   const dispatch = useDispatch();
+  const productList = useSelector(state => state.productList);
+  const { loading, error, products } = productList;
 
   useEffect(() => {
     dispatch(listProducts())
   }, [dispatch])
-  const products = [];
 
   return (
     <>
       <h1>Newest Products</h1>
+      {loading ? <h2>Loading...</h2> : error ? (
+        <h3>{error}</h3>
+      ) : (
       <Row>
         {products.map((product) => (
           <Col key={product._id} sm ={12} md={6} lg={4} xl={3} >
@@ -23,6 +26,7 @@ const HomeScreen = () => {
           </Col>
         ))}
       </Row>
+      )}
     </>
   )
 }
